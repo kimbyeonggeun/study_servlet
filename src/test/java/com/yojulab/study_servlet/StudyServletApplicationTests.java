14de0e1505dcb2e19a883693a403e27ae57d859b
kimbyeonggeun <qudrms2157@gmail.com>
2022-12-14 12:41:39 +0900

    first commit

---
+++ src/test/java/com/yojulab/study_servlet/StudyServletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yojulab.study_servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

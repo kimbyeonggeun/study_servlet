7905c00c699dc0a8de5ba0823858ebc8e90c804a
kimbyeonggeun <qudrms2157@gmail.com>
2022-12-14 12:42:40 +0900

    first commit

--- src/main/java/com/yojulab/study_servlet/HelloWorldServlets.java
+++ src/main/java/com/yojulab/study_servlet/HelloWorldServlets.java
@@ -11,15 +11,16 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = "/helloWorldServlets")
 public class HelloWorldServlets extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String message = "HelloWorldServlets!"
+
+        String message = "HelloWorldServlets with Message!";
         PrintWriter printWriter = response.getWriter();
 
         printWriter.println("<html lang='en'>");
         printWriter.println("<head>");
-        printWriter.println("<title>"+message+"</title>");
+        printWriter.println("<title>" + message + "</title>");
         printWriter.println("</head>");
         printWriter.println("<body>");
-        printWriter.println(" <div>"+message+"</div>");
+        printWriter.println(" <div>" + message + "</div>");
         printWriter.println("</body>");
         printWriter.println("</html>");
 

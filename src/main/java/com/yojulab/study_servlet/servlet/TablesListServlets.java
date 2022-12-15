9bb004787a545fea91146fdca713883b74aab323
kimbyeonggeun <qudrms2157@gmail.com>
2022-12-15 15:20:15 +0900

    add files

--- src/main/java/com/yojulab/study_servlet/servlet/TablesListServlets.java
+++ src/main/java/com/yojulab/study_servlet/servlet/TablesListServlets.java
@@ -1,27 +1,32 @@
-package com.yojulab.study_servlet;
+package com.yojulab.study_servlet.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
 
+import com.yojulab.study_servlet.DatasInfor;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/searchFromServlet")
-public class SearchFormServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+@WebServlet(urlPatterns = "/tablesListServlets")
+public class TablesListServlets extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
+        DatasInfor datasInfor = new DatasInfor();
+        ArrayList<String> tablesListWithStrng = datasInfor.getTablesListData();
         PrintWriter printWriter = response.getWriter();
 
-        DatasInfor datasInfor = new DatasInfor();
-        HashMap<String, String> searchFormData = datasInfor.getSearchFormData();
-        String title = searchFormData.get("search_key");
+        HashMap<String, String> searchForm = datasInfor.getSearchFormData();
         printWriter.println("<html lang='en'>");
-
         printWriter.println("<head>");
-        printWriter.println("<title>" + title + "</title>");
+        printWriter.println("<title>" + searchForm.get("search_key") + "</title>");
         printWriter.println("<link");
         printWriter.println("href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css'");
         printWriter.println("rel='stylesheet'");
@@ -30,18 +35,28 @@ public class SearchFormServlet extends HttpServlet {
         printWriter.println("/>");
         printWriter.println("<link rel='stylesheet' href='./css/commons.css' />");
         printWriter.println("</head>");
-
         printWriter.println("<body>");
-        printWriter.println("<form action=''>");
         printWriter.println("<div class='container'>");
-        printWriter.println("<div class='fs-3'>Search Form - " + searchFormData.get("name") + "</div>");
-        printWriter.println("<div>");
-        printWriter.println("<label for='' class='form-label'>Search with Name</label>");
-        printWriter.println("<input type='text' class='form-control' placeholder='Input Name' />");
+        printWriter.println("<div class='fs-3'>Tables Normal</div>");
+        printWriter.println("<table class='table'>");
+        printWriter.println("<thead>");
+        printWriter.println("<tr>");
+        printWriter.println("<th scope='3'>#</th>");
+        printWriter.println("<th scope=''>Handle</th>");
+        printWriter.println("</tr>");
+        printWriter.println("</thead>");
+        printWriter.println("<tbody>");
+        for (int i = 0; i < tablesListWithStrng.size(); i++) {
+            printWriter.println("<tr>");
+            int num = i + 1;
+            printWriter.println("<th scope=''>" + num + "</th>");
+            String handle = tablesListWithStrng.get(i);
+            printWriter.println("<td>" + handle + "</td>");
+            printWriter.println("</tr>");
+        }
+        printWriter.println("</tbody>");
+        printWriter.println("</table>");
         printWriter.println("</div>");
-        printWriter.println("</div>");
-        printWriter.println("</form>");
-
         printWriter.println("<script");
         printWriter.println("src='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js'");
         printWriter.println("integrity='sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4'");

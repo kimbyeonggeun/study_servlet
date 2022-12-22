a6b5e46eeba6a007b32afb8e28783784c54d05dc
kimbyeonggeun <qudrms2157@gmail.com>
2022-12-22 09:36:23 +0900

    add files

---
+++ src/main/java/com/byeonggeun/study_servlet/servlet/DetailServlets.java
@@ -0,0 +1,44 @@
+package com.byeonggeun.study_servlet.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.HashMap;
+
+import com.byeonggeun.study_servlet.dao.PollWithDB;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/polls/PollServlet")
+public class DetailServlets extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // input type
+        String questions_uid = request.getParameter("QUESTIONS_UID");
+
+        // biz with DB and Class
+        PollWithDB pollWithDB = new PollWithDB();
+        HashMap<String, Object> questions = null;
+        try {
+            questions = pollWithDB.getQuestion(questions_uid);
+            System.out.println(questions.get("QUESTIONS_UID"));
+            System.out.println(questions.get("QUESTIONS"));
+            System.out.println(questions.get("ORDERS"));
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        // output with html
+        request.setAttribute("questions", questions);
+
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/polls/details.jsp");
+        requestDispatcher.forward(request, response);
+    }
+}
